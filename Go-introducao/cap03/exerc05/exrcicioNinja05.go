275c66a8aaeacb56d36328f9149f8accccdb794e
Jonathan Santos <jonathan01347@gmail.com>
2023-04-13 18:42:26 -0300

    feat(capitulo03) adicionado exerc05

---
+++ Go-introducao/cap03/exerc05/exrcicioNinja05.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	for num := 10; num <= 100; num++ {
+		fmt.Println(num % 4)
+	}
+}
+
+/* 
+- Demonstre o resto da divisão por 4 de todos os números entre 10 e 100
+*/
