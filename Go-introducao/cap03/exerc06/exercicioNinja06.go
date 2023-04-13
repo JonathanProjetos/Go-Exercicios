28fe085d78160b00a14d51860b0cdc3b6b37da61
Jonathan Santos <jonathan01347@gmail.com>
2023-04-13 18:42:35 -0300

    feat(capitulo03) adicionado exerc06

---
+++ Go-introducao/cap03/exerc06/exercicioNinja06.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+var x bool
+
+func main() {
+	x = true
+	
+	if x {
+		fmt.Println("Meu valor é true")
+	}
+}
+
+
+/* 
+- Crie um programa que demonstre o funcionamento da declaração if.
+*/
