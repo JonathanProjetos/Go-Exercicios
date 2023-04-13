092c512514ba58a6dd36e19b5a0e2321c5ce460c
Jonathan Santos <jonathan01347@gmail.com>
2023-04-12 22:20:00 -0300

    feat(cap02)Adicionado exercicio 01

---
+++ Go-introducao/cap02/exec01/exercicio_ninja01.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main()  {
+	x:= 10
+
+	fmt.Printf("%b\n", x)
+	fmt.Printf("%d\n", x)
+	fmt.Printf("%#x\n", x)
+
+}
+
+/* 
+- Escreva um programa que mostre um número em decimal, binário, e hexadecimal.
+*/
