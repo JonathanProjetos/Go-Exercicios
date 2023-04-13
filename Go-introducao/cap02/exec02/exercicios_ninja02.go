aa9a2a76af4d69416eadd30fe07442bd6e1b553e
Jonathan Santos <jonathan01347@gmail.com>
2023-04-12 22:20:26 -0300

    feat(cap02)Adicionado exercicio 02

---
+++ Go-introducao/cap02/exec02/exercicios_ninja02.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+var x = 10
+
+func main() {
+	fmt.Println( x < 10)
+	fmt.Println( x > 10)
+	fmt.Println( x <= 10)
+	fmt.Println( x >= 10)
+	fmt.Println( x != 10)
+	fmt.Println( x == 10)
+	fmt.Println( x + 10 )
+	fmt.Println( x - 10)
+	fmt.Println( x * 10)
+	fmt.Println( x / 10)
+	fmt.Println( x % 10)
+}
+
+/* 
+- Escreva expressões utilizando os operadores, e atribua seus valores a variáveis.
+- Demonstre estes valores.
+*/
