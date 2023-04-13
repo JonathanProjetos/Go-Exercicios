36129c6510f032d571c7ca534354be2beac31498
Jonathan Santos <jonathan01347@gmail.com>
2023-04-12 22:20:34 -0300

    feat(cap02)Adicionado exercicio 03

---
+++ Go-introducao/cap02/exec03/exercicio_ninja03.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+const (
+	a = 100
+) 
+
+const b int = 10000
+
+func main () {
+	fmt.Println(a)
+	fmt.Println(b)
+
+}
+
+
+
+/* 
+- Crie constantes tipadas e não-tipadas.
+- Demonstre seus valores.
+*/
