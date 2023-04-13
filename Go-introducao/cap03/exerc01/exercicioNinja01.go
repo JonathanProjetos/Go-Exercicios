bda1f69bfe1dcbb6e2d4e182c75b790510abec69
Jonathan Santos <jonathan01347@gmail.com>
2023-04-13 18:39:56 -0300

    feat(capitulo03) adicionado exerc01

---
+++ Go-introducao/cap03/exerc01/exercicioNinja01.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 1; i <= 10000; i++ {
+		fmt.Println("coloquei", i,"na tela.")
+	}
+}
+
+/* 
+- Põe na tela: todos os números de 1 a 10000.
+*/
