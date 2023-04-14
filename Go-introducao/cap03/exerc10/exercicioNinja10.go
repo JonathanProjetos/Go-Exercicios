bd79de6df61edba596913c40396146afa3c8b1ff
Jonathan Santos <jonathan01347@gmail.com>
2023-04-14 19:18:53 -0300

    feat(cap03)Adicionado o execr10

---
+++ Go-introducao/cap03/exerc10/exercicioNinja10.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
+}
+
+/* 
+- Anote (à mão) o resultado das expressões:
+    - fmt.Println(true && true)  = true
+    - fmt.Println(true && false) = false
+    - fmt.Println(true || true) = true
+    - fmt.Println(true || false) = true
+    - fmt.Println(!true) = false
+*/
