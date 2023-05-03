b6a0c27202ee37eff3d4e772e84849eef92c9eb5
Jonathan Santos <jonathan01347@gmail.com>
2023-05-02 21:09:21 -0300

    feat(cap08)Add anotações sobre a matéria

--- Go-introducao/cap08/documentaçãoJSON.go
+++ Go-introducao/cap08/documentaçãoJSON.go
@@ -5,7 +5,10 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"sort"
 )
+
+import "golang.org/x/crypto/bcrypt"
 
 var jamesBond = pessoa {
 	Name: "James",
@@ -48,8 +51,47 @@ func main() {
 	// person4(&info2, json2)
 	// fmt.Printf("%+v",info2)
 
-	person05(jamesBond)
-	person06(&info2, json2)
+	// person05(jamesBond)
+	// person06(&info2, json2)
+
+	// sortExample()
+	// sortIntsExample()
+
+	//ordenação
+
+	// carros:= []car {
+	// 		{"grand siena", 100, 10},
+	// 		{"eco esport", 120, 30 },
+	// 		{"fusca", 200, 20},
+	// }
+	// // ordena por nome
+	// sort.Sort(ordenarNome(carros))
+	// fmt.Println("Nome:\n",carros)
+
+	// // ordena por potência
+	// sort.Sort(ordenarPotencia(carros))
+	// fmt.Println("Potência:\n",carros)
+
+	// // ordena por consumo
+	// sort.Sort(ordenarConsumo(carros))
+	// fmt.Println("Consumo:\n",carros)
+
+	// // ordena por lucro do posto
+	// sort.Sort(ordenarLucroDoPosto(carros))
+	// fmt.Println("Lucro:\n",carros)
+
+	//bcrypt
+
+	senha:= "senhaForte"
+	senha2:= "senhaFraca"
+
+	sb, err := bcrypt.GenerateFromPassword([]byte(senha), 10)
+	if err != nil {fmt.Println(err)}
+	fmt.Println(string(sb))
+	//correta return nil	
+	fmt.Println(bcrypt.CompareHashAndPassword(sb,[]byte(senha)))
+	// errada retorna um erro
+	fmt.Println(bcrypt.CompareHashAndPassword(sb,[]byte(senha2)))
 }
 
 /* 
@@ -121,3 +163,100 @@ func person06(p *pessoa, x string) {
 
 // Interface whiter
 
+/* 
+	Em Go, uma interface é um tipo abstrato que define um conjunto de métodos. 
+	A interface io.Writer é uma das interfaces mais comuns e é usada para representar um objeto que pode escrever dados em algum lugar, 
+	como na tela ou em um arquivo. Qualquer tipo que implemente os métodos da interface io.Writer pode ser usado em funções que esperam
+	um io.Writer como parâmetro, sem que precise ser especificado o tipo específico. 
+	Isso permite que o código seja mais flexível e reutilizável, pois diferentes tipos podem ser usados como entrada para as mesmas funções.
+	Além disso, em Go é possível criar interfaces vazias, que não especificam nenhum método, 
+	mas podem ser usadas para permitir que um tipo seja usado como argumento genérico em funções e métodos. 
+	Isso permite uma maior flexibilidade na escrita de código genérico e reutilizável em Go.
+*/
+
+
+// Pacote sort
+
+//O sort é um pacote para ordenação de slices
+
+func sortStringsExample() {
+		ss:= []string{"fui", "jantar", "fora", "hoje"}
+		fmt.Println(ss)
+		sort.Strings(ss)
+		fmt.Println(ss)
+}
+
+
+func sortIntsExample() {
+	nn:= []int{10,35,87,9,90,20,58}
+	fmt.Println(nn)
+	sort.Ints(nn)
+	fmt.Println(nn)
+}
+
+// Custom Sort
+
+type car struct {
+	nome string
+	potencia int
+	consumo int
+}
+
+type ordenarNome []car
+type ordenarPotencia []car
+type ordenarConsumo []car
+type ordenarLucroDoPosto []car
+
+//Por nome
+
+func (n ordenarNome) Len()int {
+	return len(n)
+}
+
+func (n ordenarNome) Swap(i , j int) {
+	n[i], n[j] = n[j], n[i]
+}
+
+func (n ordenarNome) Less(i, j int)bool {
+	return n[i].nome < n[j].nome
+}
+
+//por potência
+
+func (p ordenarPotencia) Len()int {
+	return len(p)
+}
+
+func (p ordenarPotencia) Swap(i , j int) {
+	p[i], p[j] = p[j], p[i]
+}
+
+func (p ordenarPotencia) Less(i, j int)bool {
+	return p[i].potencia < p[j].potencia
+}
+
+//por consumo
+
+func (c ordenarConsumo) Len()int {
+	return len(c)
+}
+
+func (c ordenarConsumo) Swap(i , j int) {
+	c[i], c[j] = c[j], c[i]
+}
+
+func (c ordenarConsumo) Less(i, j int)bool {
+	return c[i].consumo > c[j].consumo
+}
+
+func (l ordenarLucroDoPosto) Len()int {
+	return len(l)
+}
+
+func (l ordenarLucroDoPosto) Swap(i , j int) {
+	l[i], l[j] = l[j], l[i]
+}
+
+func (l ordenarLucroDoPosto) Less(i, j int)bool {
+	return l[i].consumo > l[j].consumo
+}
